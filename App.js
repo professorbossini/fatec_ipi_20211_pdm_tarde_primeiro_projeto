1f21c5fe88e550578e037fdfe9b2f619964eb42f
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-02-26 16:12:52 -0300

    Terminamos o pdf 02

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor (props){
+    super(props);
+    this.state = {
+      meuTexto: "Um texto inicial",
+      contador: 0
+    }
+  }
+  render (){
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.contador}</Text>
+        <Button
+          title="Incrementar"
+          onPress={ _ => this.setState({contador: this.state.contador + 1})}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//hook
+/*export default function App() {
+  
+  const [outputText, setOutputText] = useState('Texto inicial');
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button 
+        title="Alterar texto"
+        onPress={() => {setOutputText('Novo texto')}}
+      />
+    </View>
+  );
+}*/
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  }
+);
